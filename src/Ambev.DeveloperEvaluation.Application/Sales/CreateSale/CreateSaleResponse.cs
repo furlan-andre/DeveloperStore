60cd7dbcf1a4a24e0fd0100eeecef3380cd99dc9
Andre Furlan <furlan.andre91@gmail.com>
2026-05-09 20:44:11 -0400

    feature004(feat): Add CreateSaleResponse

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResponse.cs
@@ -0,0 +1,17 @@
+using Ambev.DeveloperEvaluation.Application.Sales.CreateSaleItem;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+
+public class CreateSaleResponse
+{
+    public Guid Id { get; set; }
+    public string SaleNumber { get; set; } = string.Empty;
+    public DateTime SaleDate { get; set; }
+    public Guid CustomerId { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public Guid BranchId { get; set; }
+    public string BranchName { get; set; } = string.Empty;
+    public decimal TotalSaleAmount { get; set; }
+    public bool Active { get; set; }
+    public IReadOnlyCollection<CreateSaleItemResponse> Items { get; set; } = [];
+}
