5f7402b724a519db3886d08008686b64535a63b9
Andre Furlan <furlan.andre91@gmail.com>
2026-05-10 12:18:16 -0400

    feature006(feat): add logic dele in SaleRepository

--- src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
@@ -7,4 +7,5 @@ public interface ISaleRepository
     Task AddAsync(Sale sale, CancellationToken cancellationToken = default);
     Task<Sale?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
     Task UpdateAsync(Sale sale, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Sale sale, CancellationToken cancellationToken = default);
 }
